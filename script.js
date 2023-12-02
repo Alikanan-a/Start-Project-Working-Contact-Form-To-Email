42743a5ba0a7acb192d56ddbd8a307d9122bb16e
alikanan <a.kanan2222@gmail.com>
2023-12-02 22:44:03 +0330

    add cdn and api

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+const form =  document.querySelector('form'); 
+function sendEmail(){  
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "ali.kanan446@gmail.com",
+        Password : "6FB45F1C8F00C98F56D37DE6CFCF92680A43",
+        To : 'ali.kanan446@gmail.com',
+        From : "ali.kanan446@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
